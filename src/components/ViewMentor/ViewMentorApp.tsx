1ce449dce651938fdddbeb2171abefb5905ff764
betich <betich365@gmail.com>
2022-08-19 08:39:32 +0700

    feat: implement view sessions card in mentor page

--- src/components/ViewMentor/ViewMentorApp.tsx
+++ src/components/ViewMentor/ViewMentorApp.tsx
@@ -1,4 +1,5 @@
-import { ArrowLeftIcon, BriefcaseIcon, IdentificationIcon } from "@heroicons/react/solid"
+import { ArrowLeftIcon, BriefcaseIcon, CalendarIcon, IdentificationIcon } from "@heroicons/react/solid"
+import clsx from "clsx"
 import Link from "next/link"
 
 const ContactSection = () => {
@@ -39,6 +40,48 @@ const WorkExperienceSection = () => {
   )
 }
 
+interface DateElementProps {
+  day: string
+  date: number
+  month: string
+  selected?: boolean
+}
+
+const DateElement = ({ day, date, month, selected }: DateElementProps) => {
+  return (
+    <button
+      className={clsx(
+        "flex flex-col items-center text-gray-700 transition-colors hover:bg-gray-100 p-4 rounded-2xl",
+        selected && "bg-gray-100"
+      )}
+    >
+      <p className="text-xs font-light">{day}</p>
+      <p className="text-lg">{date}</p>
+      <p className="text-xs font-light">{month}</p>
+    </button>
+  )
+}
+
+interface SessionElementProps {
+  time: string
+  date: string
+  booked?: boolean
+}
+
+const SessionElement = ({ time, date, booked }: SessionElementProps) => {
+  return (
+    <button className="rounded-lg text-left w-full border border-l-8 border-gray-700">
+      <div className="flex flex-col p-4">
+        <div className="flex items-center justify-between">
+          <p className="text-lg">{time}</p>
+          <p className="text-sm font-light">{booked && "Booked"}</p>
+        </div>
+        <p className="font-light">{date}</p>
+      </div>
+    </button>
+  )
+}
+
 const ViewMentorApp = () => {
   return (
     <main className="h-full flex flex-col gap-4">
@@ -64,7 +107,28 @@ const ViewMentorApp = () => {
         <ContactSection />
         <WorkExperienceSection />
 
-        <h2 className="mt-6">Available Sessions</h2>
+        <h2 className="mt-6 mb-4">Available Sessions</h2>
+
+        <section className="flex items-center gap-3 px-6 w-full overflow-x-auto">
+          <div className="pr-4">
+            <CalendarIcon className="w-6 h-6 text-gray-700" />
+          </div>
+
+          <DateElement day="THU" date={10} month="SEP" selected />
+          <DateElement day="THU" date={11} month="SEP" />
+          <DateElement day="THU" date={12} month="SEP" />
+          <DateElement day="THU" date={13} month="SEP" />
+          <DateElement day="THU" date={14} month="SEP" />
+          <DateElement day="THU" date={15} month="SEP" />
+          <DateElement day="THU" date={16} month="SEP" />
+          <DateElement day="THU" date={17} month="SEP" />
+        </section>
+
+        <section className="flex flex-col items-center justify-center px-2 gap-4 pt-6">
+          <SessionElement time="20.00-20.30 น." date="11 SEP 2022" booked />
+          <SessionElement time="20.00-20.30 น." date="11 SEP 2022" />
+          <SessionElement time="20.00-20.30 น." date="11 SEP 2022" />
+        </section>
       </section>
     </main>
   )
