c96cbd269c81b851c8ce2e4d3371e8775b273be8
betich <betich365@gmail.com>
2022-10-11 21:27:20 +0700

    fix: fix imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import "@styles/tailwind.css"
 import "@styles/fonts.css"
 import type { AppProps } from "next/app"
 import AppHeader from "@components/common/Header"
-import MobileContainer from "@components/common/MobileContainer"
 import Navigation from "@components/common/Navigation"
 
 function MyApp({ Component, pageProps }: AppProps) {
